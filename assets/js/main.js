fa2bb802a4f4d7730159a06d3f31bcdb2fb31e79
Pola <Pola.kinomoto@gmail.com>
2017-05-22 00:25:04 -0400

    algo agregado

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,9 @@ for (var i = 0; i < borrar.length; i++) {
     hid.addEventListener("click", deleteF);
 }
 
+var pictures = ["assets/img/primer.jpg","assets/img/segund.jpg", "assets/img/leng.jpg", "assets/img/tercer.jpg"];
+ var content= document.getElementById("01");
+ var img= document.createElement("img");
+ img.setAttribute("src", pictures[0]);
+ img.setAttribute("class", "imagen");
+ content.appendChild(img);
